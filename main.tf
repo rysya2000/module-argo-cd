7cac112b24d4da81b0889518056f173e9785bc18
rysya2000 <rysaldy.kayipbergen@gmail.com>
2023-09-05 17:59:28 +0600

    ArgoCD module reinit (without 'load_config_file' field No4)

--- main.tf
+++ main.tf
@@ -1,4 +1,5 @@
 provider "kubernetes" {
+  # load_config_file       = false
   cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
   host                   = var.kubernetes_cluster_endpoint
   exec {
@@ -7,10 +8,9 @@ provider "kubernetes" {
     args        = ["token", "-i", "${var.kubernetes_cluster_name}"]
   }
 }
-
 provider "helm" {
   kubernetes {
-
+    # load_config_file       = false
     cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
     host                   = var.kubernetes_cluster_endpoint
     exec {
@@ -21,56 +21,14 @@ provider "helm" {
   }
 }
 
-
-/*
-provider "aws" {
-  region = var.aws_region
-}
-
-data "aws_eks_cluster" "msur" {
-  name = var.kubernetes_cluster_id
-}
-
-provider "kubernetes" {
-  load_config_file       = false
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.msur.certificate_authority.0.data)
-  host                   = data.aws_eks_cluster.msur.endpoint
-  exec {
-    api_version = "client.authentication.k8s.io/v1alpha1"
-    command     = "aws-iam-authenticator"
-    args        = ["token", "-i", "${data.aws_eks_cluster.msur.name}"]
-  }
-}
-
-
-
-provider "helm" {
-  kubernetes {
-    load_config_file       = false
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.msur.certificate_authority.0.data)
-    host                   = data.aws_eks_cluster.msur.endpoint
-    exec {
-      api_version = "client.authentication.k8s.io/v1alpha1"
-      command     = "aws-iam-authenticator"
-      args        = ["token", "-i", "${data.aws_eks_cluster.msur.name}"]
-    }
-  }
-}
-*/
-
-resource "kubernetes_namespace" "argo-ns" {
+resource "kubernetes_namespace" "example" {
   metadata {
-    name = "argocd"
+    name = "argo"
   }
 }
-
 resource "helm_release" "argocd" {
   name       = "msur"
   chart      = "argo-cd"
   repository = "https://argoproj.github.io/argo-helm"
-  namespace  = "argocd"
-
-  # We are going to access the console with a port forwarded connection, so we'll disable TLS.
-  # This allow us to avoid the self-signed certificate warning for localhosts.
-  # controller.extraArgs = ["insecure"]
+  namespace  = "argo"
 }
