00966e7a3e5913d859c812a6601d5d898413dcc0
rysya2000 <rysaldy.kayipbergen@gmail.com>
2023-09-12 16:39:47 +0600

    asd

--- main.tf
+++ main.tf
@@ -1,34 +1,39 @@
 provider "kubernetes" {
-  # load_config_file       = false
   cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
   host                   = var.kubernetes_cluster_endpoint
   exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
+    api_version = "client.authentication.k8s.io/v1alpha1"
     command     = "aws-iam-authenticator"
     args        = ["token", "-i", "${var.kubernetes_cluster_name}"]
   }
 }
+
 provider "helm" {
   kubernetes {
-    # load_config_file       = false
     cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
     host                   = var.kubernetes_cluster_endpoint
     exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
+      api_version = "client.authentication.k8s.io/v1alpha1"
       command     = "aws-iam-authenticator"
       args        = ["token", "-i", "${var.kubernetes_cluster_name}"]
     }
   }
 }
 
-resource "kubernetes_namespace" "example" {
+
+resource "kubernetes_namespace" "argo-ns" {
   metadata {
-    name = "argo"
+    name = "argocd"
   }
 }
+
 resource "helm_release" "argocd" {
   name       = "msur"
   chart      = "argo-cd"
   repository = "https://argoproj.github.io/argo-helm"
-  namespace  = "argo"
+  namespace  = "argocd"
+
+  # We are going to access the console with a port forwarded connection, so we'll disable TLS.
+  # This allow us to avoid the self-signed certificate warning for localhosts.
+  # controller.extraArgs = ["insecure"]
 }
